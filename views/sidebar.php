cbfa5aa334689b0df4e0f4a1417d124a7f0f0d92
Darrin C. Roenfanz <darrin.roenfanz@gmail.com>
2011-10-28 06:17:00 -0700

    Replace PHP short-tags with regular ones.

--- views/sidebar.php
+++ views/sidebar.php
@@ -11,9 +11,9 @@
 if (!defined('IN_CMS')) { exit(); }
 
 ?>
-<p class="button"><a href="<?=get_url('plugin/banner'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/banner/images/list.png" align="middle" /><?php echo __('List Banners'); ?></a></p>
-<p class="button"><a href="<?=get_url('plugin/banner/banner_add'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/banner/images/new.png" align="middle" /><?php echo __('Add Banner'); ?></a></p>
-<p class="button"><a href="<?=get_url('plugin/banner/documentation'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/banner/images/documentation.png" align="middle" /><?php echo __('Documentation'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/banner'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/banner/images/list.png" align="middle" /><?php echo __('List Banners'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/banner/banner_add'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/banner/images/new.png" align="middle" /><?php echo __('Add Banner'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/banner/documentation'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/banner/images/documentation.png" align="middle" /><?php echo __('Documentation'); ?></a></p>
 <div class="box">
 <h2><?php echo __('Banners Plugin');?></h2>
 <p>
