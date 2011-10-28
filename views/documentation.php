0133172619c81ebbd9062519a96c5731068c011e
Darrin C. Roenfanz <darrin.roenfanz@gmail.com>
2011-10-28 06:19:53 -0700

    Replace `&lt`; and `&gt`; with `<` and `>`, respectively.

--- views/documentation.php
+++ views/documentation.php
@@ -8,19 +8,18 @@
  */
 
 // Security Measure
-if (!defined('IN_CMS')) { exit(); }
-
+    if (!defined('IN_CMS')) { exit(); }
 ?>
 <h1><?php echo __('Documentation'); ?></h1>
 
 <h2>Static Usage</h2>
 <p>Load a particular banner.</p>
-<code>&lt;?php echo bannerById('id_of_div','banner_id'); ?&gt;</code>
+<code><?php echo bannerById('id_of_div','banner_id'); ?></code>
 
 <h2>Dynamic Usage (single load)</h2>
 <p>Load a dynamic banner once per page load.</p>
-<code>&lt;?php echo bannerBySize('id_of_div','banner_width','banner_height'); ?&gt;</code>
+<code><?php echo bannerBySize('id_of_div','banner_width','banner_height'); ?></code>
 
 <h2>Dynamic Usage (dynamic)</h2>
 <p>Load a dynamic banner that refreshes ever X seconds.</p>
-<code>&lt;?php echo bannerBySizeDynamic('id_of_div','banner_width','banner_height','timeout'); ?&gt;</code>
+<code><?php echo bannerBySizeDynamic('id_of_div','banner_width','banner_height','timeout'); ?></code>
